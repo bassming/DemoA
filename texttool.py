db7d0291ab60b59797c5265b74d4655a2c0c615e
GNRMCR <18377130@buaa.edu.cn>
2020-03-01 10:34:10 +0800

    B initial update

--- texttool.py
+++ texttool.py
@@ -61,3 +61,52 @@ def keyword():
         height=rect.get_height()
         plt.text(rect.get_x()+rect.get_width()/2,1.05*height,'%d'%int(height))
     plt.show()
+def menu1():
+    ttt=t.get(1.0,END)
+    ttt1=ttt.split()
+    r=simpledialog.askstring('单词个数查找','请输入要查找的单词')
+    tkinter.messagebox.showinfo('单词个数查找','单词'+r+'的个数是'+str(ttt1.count(r)))
+    lineend=t.index(END)
+    lisline=lineend.split('.')
+    linenum=int(lisline[0])
+    for i in range(1,linenum+1):
+        content=t.get(str(i)+'.0',str(i)+'.end')
+        liscon=content.split()
+        for k in range(len(liscon)):
+            if liscon[k] == r:
+                temp=0
+                for m in range(k):
+                    temp+=len(liscon[m])
+                t.tag_add(str(temp)+str(i),str(i)+'.'+str(temp+k),str(i)+'.'+str(temp+k+len(r)))
+                t.tag_config(str(temp)+str(i),background='red',foreground='white')
+def menu2():
+    t0=simpledialog.askstring('单词替换','请输入需要替换的单词')
+    t1=simpledialog.askstring('单词替换','请输入替换后的单词')
+    t11=t.get(1.0,END)
+    t111=t11.split()
+    for i in range(len(t111)):
+        if t111[i]==t0:
+            t111[i]=t1
+    changet=' '.join(t111)
+    t.delete(1.0,END)
+    t.insert(1.0,changet)
+    tkinter.messagebox.showinfo('单词替换','替换成功')
+def menu3():
+    u=simpledialog.askstring('删除单词','请输入您要删除的单词')
+    s2=t.get(1.0,END)
+    liss2=s2.split()
+    for item in liss2:
+        if item==u:
+            liss2.remove(item)
+    s22=' '.join(liss2)
+    t.delete(1.0,END)
+    t.insert(1.0,s22)
+    tkinter.messagebox.showinfo('删除单词','删除成功')
+def back():
+    t.edit_undo()
+def save():
+    way=simpledialog.askstring('文本另存为','请输入文件路径')
+    context=t.get(1.0,END)
+    savetext=open(way,'w+')
+    savetext.write(context)
+    savetext.close()
